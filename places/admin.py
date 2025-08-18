424671f4e15d00959fef707d0abc17abbccdc9e5
Jana <yanix2x2@gmail.com>
2025-08-18 20:29:36 +0300

    add Sortable

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableAdminBase, SortableInlineAdminMixin
 
 from .models import Place, Image
 
 
-class ImageInlineAdmin(admin.TabularInline):
+class ImageInlineAdmin(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 1
+    fields = ('image_preview', 'picture', 'serial_number')
     readonly_fields = ('image_preview',)
 
     def image_preview(self, obj):
@@ -18,7 +20,7 @@ class ImageInlineAdmin(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [
         ImageInlineAdmin,
     ]
--- places/models.py
+++ places/models.py
@@ -15,7 +15,10 @@ class Place(models.Model):
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     picture = models.ImageField("Картинка", upload_to="", blank=True)
-    serial_number = models.IntegerField("Порядковый номер", default=0)
+    serial_number = models.PositiveIntegerField("Порядковый номер", default=0, blank=False, null=False)
+
+    class Meta:
+        ordering = ["serial_number", ]
 
     def __str__(self):
         return f'{self.serial_number} {self.place.title}'
