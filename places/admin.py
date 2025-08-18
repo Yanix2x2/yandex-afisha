ade2ae4e13ce9131edb67d81aad7b00281d2c806
Jana <yanix2x2@gmail.com>
2025-08-18 19:52:59 +0300

    add preview for pictures

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Place, Image
 
 
-class ImageInline(admin.TabularInline):
+class ImageInlineAdmin(admin.TabularInline):
     model = Image
+    extra = 1
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.picture:
+            return mark_safe(f'<img src="{obj.picture.url}" style="max-height: 200px;" />')
+        return "No image"
+    
+    image_preview.short_description = "Preview"
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
-        ImageInline,
+        ImageInlineAdmin,
     ]
 
 
